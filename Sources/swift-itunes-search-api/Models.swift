bdc6e7b8925a1a2630f66864efd137d1ec91ae5c
Dr. Michael Lauer <mickey@vanille-media.de>
2024-10-09 17:22:07 +0200

    first version somewhat working

--- Sources/swift-itunes-search-api/Models.swift
+++ Sources/swift-itunes-search-api/Models.swift
@@ -1,62 +1,70 @@
 // swift-itunes-search-api (C) Dr. Michael 'Mickey' Lauer
 
+import Foundation
+
 public enum iTunesSearchAPI {
 
-    public struct AppDetails: Codable {
-        let advisories: [String]
-        let appletvScreenshotUrls: [String]
-        let artistId: Int
-        let artistName: String
-        let artistViewUrl: String
-        let artworkUrl100: String
-        let artworkUrl512: String
-        let artworkUrl60: String
-        let averageUserRating: Double
-        let averageUserRatingForCurrentVersion: Double
-        let bundleId: String
-        let contentAdvisoryRating: String
-        let currency: String
-        let currentVersionReleaseDate: String
-        let description: String
-        let features: [String]
-        let fileSizeBytes: String
-        let formattedPrice: String
-        let genreIds: [String]
-        let genres: [String]
-        let ipadScreenshotUrls: [String]
-        let isGameCenterEnabled: Bool
-        let isVppDeviceBasedLicensingEnabled: Bool
-        let kind: String
-        let languageCodesISO2A: [String]
-        let minimumOsVersion: String
-        let price: Double
-        let primaryGenreId: Int
-        let primaryGenreName: String
-        let releaseDate: String
-        let releaseNotes: String
-        let screenshotUrls: [String]
-        let sellerName: String
-        let supportedDevices: [String]
-        let trackCensoredName: String
-        let trackContentRating: String
-        let trackId: Int
-        let trackName: String
-        let trackViewUrl: String
-        let userRatingCount: Int
-        let userRatingCountForCurrentVersion: Int
-        let version: String
-        let wrapperType: WrapperType
+    public static let baseURL: String = "https://itunes.apple.com/lookup?bundleId="
+
+    @frozen public struct AppDetails: Codable {
+        public let advisories: [String]
+        public let appletvScreenshotUrls: [String]
+        public let artistId: Int
+        public let artistName: String
+        public let artistViewUrl: String
+        public let artworkUrl100: String
+        public let artworkUrl512: String
+        public let artworkUrl60: String
+        public let averageUserRating: Double
+        public let averageUserRatingForCurrentVersion: Double
+        public let bundleId: String
+        public let contentAdvisoryRating: String
+        public let currency: String
+        public let currentVersionReleaseDate: String
+        public let description: String
+        public let features: [String]
+        public let fileSizeBytes: String
+        public let formattedPrice: String
+        public let genreIds: [String]
+        public let genres: [String]
+        public let ipadScreenshotUrls: [String]
+        public let isGameCenterEnabled: Bool
+        public let isVppDeviceBasedLicensingEnabled: Bool
+        public let kind: String
+        public let languageCodesISO2A: [String]
+        public let minimumOsVersion: String
+        public let price: Double
+        public let primaryGenreId: Int
+        public let primaryGenreName: String
+        public let releaseDate: String
+        public let releaseNotes: String
+        public let screenshotUrls: [String]
+        public let sellerName: String
+        public let supportedDevices: [String]
+        public let trackCensoredName: String
+        public let trackContentRating: String
+        public let trackId: Int
+        public let trackName: String
+        public let trackViewUrl: String
+        public let userRatingCount: Int
+        public let userRatingCountForCurrentVersion: Int
+        public let version: String
+        public let wrapperType: WrapperType
     }
 
-    public struct AppStoreResponse: Codable {
-        let resultCount: Int
-        let results: [AppDetails]
+    @frozen public struct AppStoreResponse: Codable {
+        public let resultCount: Int
+        public let results: [AppDetails]
     }
 
-    public enum WrapperType: String, Codable {
+    @frozen public enum WrapperType: String, Codable {
         case track
         case collection
         case artist
         case software
+    }
+
+    public static func urlForApp(with bundleId: String) -> URL {
+        return URL(string: "\(baseURL)\(bundleId)")!
     }
 }
--- Sources/swift-itunes-search/App.swift
+++ Sources/swift-itunes-search/App.swift
@@ -2,14 +2,34 @@
 
 import ArgumentParser
 import CornucopiaHTTP
+import Foundation
 import swift_itunes_search_api
 
 @main
-struct Search: ParsableCommand {
+struct Search: AsyncParsableCommand {
+
     @Argument(help: "The bundle id to lookup.")
     var bundleId: String
 
-    mutating func run() throws {
+    mutating func run() async throws {
         print("Looking up \(bundleId)...")
+
+        let url = iTunesSearchAPI.urlForApp(with: self.bundleId)
+        let urlRequest = URLRequest(url: url)
+        let response: iTunesSearchAPI.AppStoreResponse = try await urlRequest.GET()
+
+        switch response.resultCount {
+            case 0:
+                print("Nothing found for \(bundleId).")
+
+            case 1:
+                let app = response.results.first!
+                print("Found \(app.trackName) by \(app.artistName).")
+                print("Latest version is \(app.version).")
+                print("AppleId is \(app.trackId), navigation URL is \(app.trackViewUrl)")
+
+            default:
+                print("Found \(response.resultCount) apps for \(bundleId)?")
+        }
     }
 }
